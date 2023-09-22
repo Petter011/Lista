ff25801da86623967a985388b14795b23c7ab802
Petter <petter.gustafsson@hotmail.com>
2023-09-22 22:25:02 +0200

    Lista

--- App.js
+++ App.js
@@ -1,20 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [inputText, setInputText] = useState('');
+  const [dataArray, setDataArray] = useState([]);
+
+  const handleAddText = () => {
+    setDataArray(prevDataArray => [...prevDataArray, inputText]);
+    setInputText('');
+  };
+
+  const DATA = [
+    {
+      key: "Manchester United",
+    },
+    {
+      key: "Liverpool",
+    },
+    {
+      key: "Barca",
+    },
+    {
+      key: "Real Madrid",
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headline}>
+        <Text style={styles.headlineText}>Nu gör vi en lista!</Text>
+      </View>
+
+      <View style={styles.listView}>
+        <FlatList
+          data={dataArray}
+          renderItem={({ item }) => (
+            <Text style={styles.listText}>{item}</Text>
+          )}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={text => setInputText(text)}
+          value={inputText}
+          placeholder="Skriv här"
+          keyboardType="numeric"
+        />
+      </View>
+      <Button
+        title="Lägg till"
+        onPress={handleAddText}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  headline: {
+    flex: 1,
+    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: 50,
+  },
+  headlineText: {
+    fontSize: 40,
+  },
+  listView: {
+    flex: 2,
+    paddingTop: 60,
+    justifyContent: "center",
+  },
+  listText: {
+    fontSize: 20,
+    justifyContent: "center",
+    padding: 5,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    margin: 20,
+    borderWidth: 4,
+    padding: 10,
   },
 });
